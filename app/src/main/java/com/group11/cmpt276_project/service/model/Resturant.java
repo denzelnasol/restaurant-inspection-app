ae91dd1fe41c8543743f30a44158d58500de02f9
Karamveer <stuntmania101@gmail.com>
2020-10-29 12:21:46 -0700

    Restaurant Model class implemented

--- app/src/main/java/com/group11/cmpt276_project/service/model/Resturant.java
+++ app/src/main/java/com/group11/cmpt276_project/service/model/Resturant.java
@@ -1,5 +1,67 @@
 package com.group11.cmpt276_project.service.model;
 
 public class Resturant {
-    //ff
+    String trackingNumber;
+    String name;
+    String physicalAddress;
+    String physicalCity;
+    String facilityType;
+    double latitude;
+    double longitude;
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhysicalAddress(String physicalAddress) {
+        this.physicalAddress = physicalAddress;
+    }
+
+    public void setPhysicalCity(String physicalCity) {
+        this.physicalCity = physicalCity;
+    }
+
+    public void setFacilityType(String facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhysicalAddress() {
+        return physicalAddress;
+    }
+
+    public String getPhysicalCity() {
+        return physicalCity;
+    }
+
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
