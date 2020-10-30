2206c6c1a65c48be09d413719b54ce73419819e0
wenhaoz <wenhao_zhang@sfu.ca>
2020-10-29 23:18:52 -0700

    Add call to transition to restaurant list activity

--- app/src/main/java/com/group11/cmpt276_project/view/ui/MainActivity.java
+++ app/src/main/java/com/group11/cmpt276_project/view/ui/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.init();
-
+        this.moveToRestaurantList();
     }
 
     private void moveToRestaurantList() {
