413b4ed431dc66e2ab636975aaaa3fe7543b2c52
Jacqueline Lee <jsl59@sfu.ca>
2020-10-30 21:24:15 -0700

    Implement repository to load the inspection report json file
    The key is a string of the tracking number and the value is an array of the report details.

--- app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
+++ app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
@@ -3,4 +3,5 @@ package com.group11.cmpt276_project.utils;
 public class Constants {
 
     public static String ENCODING = "UTF-8";
+    public static final String INSPECTION_REPORT_FILE = "inspectionReports.json";
 }
