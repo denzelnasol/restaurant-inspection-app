5ef9805307b2f985cee0cfd1602c1be3b791e7b6
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-10-30 21:09:16 -0700

    Add ViolationRepository

--- app/src/main/java/com/group11/cmpt276_project/service/repository/ViolationRepository.java
+++ app/src/main/java/com/group11/cmpt276_project/service/repository/ViolationRepository.java
@@ -1,4 +1,31 @@
 package com.group11.cmpt276_project.service.repository;
 
+import android.content.Context;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.group11.cmpt276_project.service.model.Violation;
+import com.group11.cmpt276_project.utils.Constants;
+import com.group11.cmpt276_project.utils.Utils;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class ViolationRepository {
+    private Context context;
+    private ObjectMapper objectMapper;
+
+    public ViolationRepository(Context context) {
+        this.context = context;
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public Map<String, List<Violation>> get() throws IOException {
+        String jsonString = Utils.getJsonFromAssets(this.context, Constants.VIOLATION_FILE);
+        TypeReference<Map<String, List<Violation>>> mapTypeReference = new TypeReference<Map<String, List<Violation>>>() {};
+        return objectMapper.readValue(jsonString, mapTypeReference);
+    }
 }
