86e4117e9f5da856fde8f34893e5393f6af6a9dc
Jacqueline Lee <jsl59@sfu.ca>
2020-11-01 18:14:52 -0800

    Add adapter for RecyclerView and DataBinding

--- app/src/main/java/com/group11/cmpt276_project/view/ui/InspectionDetailActivity.java
+++ app/src/main/java/com/group11/cmpt276_project/view/ui/InspectionDetailActivity.java
@@ -1,16 +1,20 @@
 package com.group11.cmpt276_project.view.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.group11.cmpt276_project.R;
 import com.group11.cmpt276_project.databinding.ActivityInspectionDetailBinding;
 import com.group11.cmpt276_project.service.model.InspectionReport;
 import com.group11.cmpt276_project.service.model.Violation;
+import com.group11.cmpt276_project.view.adapter.InspectionDetailAdapter;
+import com.group11.cmpt276_project.view.adapter.interfaces.IItemOnClick;
 import com.group11.cmpt276_project.viewmodel.InspectionReportsViewModel;
 import com.group11.cmpt276_project.viewmodel.RestaurantsViewModel;
 import com.group11.cmpt276_project.viewmodel.ViolationsViewModel;
@@ -37,6 +41,7 @@ public class InspectionDetailActivity extends AppCompatActivity {
     private InspectionReportsViewModel inspectionReportsViewModel;
     private RestaurantsViewModel restaurantsViewModel;
     private InspectionReport inspectionReport;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +71,9 @@ public class InspectionDetailActivity extends AppCompatActivity {
         ActivityInspectionDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_inspection_detail);
         binding.setReport(this.inspectionReport);
         binding.setActivity(this);
-    };
+
+        this.recyclerView = findViewById(R.id.violation_list);
+    }
 
     private void observeVisibility() {
         List<Violation> violationList = this.getViolationList();
@@ -81,6 +88,21 @@ public class InspectionDetailActivity extends AppCompatActivity {
             violationList.add(this.violationsViewModel.get(String.valueOf(id)));
         }
 
+        InspectionDetailAdapter inspectionDetailAdapter = new InspectionDetailAdapter(violationList, new ViolationsOnClick()
+                );
+
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        this.recyclerView.setAdapter(inspectionDetailAdapter);
+
         return violationList;
+    }
+
+    private class ViolationsOnClick implements IItemOnClick {
+
+        @Override
+        public void onItemClick(int position) {
+            Intent intent = InspectionDetailActivity.startActivity(InspectionDetailActivity.this, 0, position);
+            startActivity(intent);
+        }
     }
 }
