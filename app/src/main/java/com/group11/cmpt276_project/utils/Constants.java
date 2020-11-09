8b13791e727c673d4a43ebbc13a3e29d62624934
Jacqueline Lee <jsl59@sfu.ca>
2020-11-09 15:28:55 -0800

    Add constant strings of restaurant names to Constants.java

--- app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
+++ app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
@@ -16,5 +16,20 @@ public class Constants {
     public static final String TRACKING_NUMBER = "trackingNumber";
     public static final String INDEX = "index";
     public static final String PARENT = "parent";
+    public static final String SEVEN_ELEVEN = "7eleven";
+    public static final String AW = "A&W";
+    public static final String BOOSTER_JUICE = "Booster Juice";
+    public static final String BROWNS = "Browns";
+    public static final String CHURCHS_CHICKEN = "Churchs Chicken";
+    public static final String DOMINOS = "Dominos";
+    public static final String DAIRY_QUEEN = "Dairy Queen";
+    public static final String MCDONALDS = "Mcdonalds";
+    public static final String PIZZAHUT = "Pizzahut";
+    public static final String REAL_CANADIAN = "Real Canadian";
+    public static final String STARBUCKS = "Starbucks";
+    public static final String SUBWAY = "Subway";
+    public static final String TIM_HORTONS = "Tim Hortons";
+    public static final String WALMART = "Walmart";
+    public static final String WENDYS = "Wendys";
 
 }
