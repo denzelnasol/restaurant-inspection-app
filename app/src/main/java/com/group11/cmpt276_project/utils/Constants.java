b9b8b7b7038696dc0e34987e88b608102fa89602
Jacqueline Lee <jsl59@sfu.ca>
2020-11-09 17:27:50 -0800

    Minor changes to Constants

--- app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
+++ app/src/main/java/com/group11/cmpt276_project/utils/Constants.java
@@ -16,20 +16,20 @@ public class Constants {
     public static final String TRACKING_NUMBER = "trackingNumber";
     public static final String INDEX = "index";
     public static final String PARENT = "parent";
-    public static final String SEVEN_ELEVEN = "7eleven";
+    public static final String SEVEN_ELEVEN = "7-Eleven";
     public static final String AW = "A&W";
     public static final String BOOSTER_JUICE = "Booster Juice";
-    public static final String BROWNS = "Browns";
-    public static final String CHURCHS_CHICKEN = "Churchs Chicken";
-    public static final String DOMINOS = "Dominos";
+    public static final String BROWNS = "Browns Socialhouse";
+    public static final String CHURCHS_CHICKEN = "Church's Chicken";
+    public static final String DOMINOS = "Domino's Pizza";
     public static final String DAIRY_QUEEN = "Dairy Queen";
-    public static final String MCDONALDS = "Mcdonalds";
-    public static final String PIZZAHUT = "Pizzahut";
-    public static final String REAL_CANADIAN = "Real Canadian";
+    public static final String MCDONALDS = "Mcdonald's";
+    public static final String PIZZAHUT = "Pizza Hut";
+    public static final String REAL_CANADIAN = "Real Canadian Superstore";
     public static final String STARBUCKS = "Starbucks Coffee";
     public static final String SUBWAY = "Subway";
     public static final String TIM_HORTONS = "Tim Hortons";
-    public static final String WALMART = "Walmart";
+    public static final String WALMART = "Wal-mart";
     public static final String WENDYS = "Wendy's";
 
 }
