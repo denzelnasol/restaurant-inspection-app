a0b1fd93aeb415234bd379080029454af07e3695
Jacqueline Lee <jsl59@sfu.ca>
2020-11-15 20:26:01 -0800

    Change Progress Bar size

--- app/src/main/java/com/group11/cmpt276_project/service/repository/IPreferenceRepository.java
+++ app/src/main/java/com/group11/cmpt276_project/service/repository/IPreferenceRepository.java
@@ -3,6 +3,5 @@ package com.group11.cmpt276_project.service.repository;
 public interface IPreferenceRepository {
 
     String getPreference(String preference);
-
     void savePreference(String preference, String value);
 }
