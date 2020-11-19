fccf22f7bb4b669bde1fed692ff1826830bffa51
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-11-18 22:45:03 -0800

    Remove logs

--- app/src/main/java/com/group11/cmpt276_project/view/ui/fragment/MapFragment.java
+++ app/src/main/java/com/group11/cmpt276_project/view/ui/fragment/MapFragment.java
@@ -121,8 +121,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     }
                 }
             });
-
-            Log.d("TEST", "TEST");
         }
     };
 
